a81661992fd7432a2c23c29e74fdb166ca7a42c4
Qiu2025 <siyuanqiu123@gmail.com>
2026-04-30 01:01:36 +0200

    style(ui): increase contrast and add brand-colored borders to toast notifications for better visibility

--- components/toast.tsx
+++ components/toast.tsx
@@ -11,9 +11,9 @@ const Toaster = ({ ...props }: ToasterProps) => {
       toastOptions={{
         classNames: {
           toast:
-            "group toast group-[.toaster]:bg-background group-[.toaster]:text-gray-12 group-[.toaster]:border-gray-3 group-[.toaster]:shadow-lg",
+            "group toast group-[.toaster]:bg-gray-4 group-[.toaster]:text-white group-[.toaster]:border-brand/50 group-[.toaster]:shadow-[0_0_20px_rgba(255,0,0,0.1)]",
           description: "group-[.toast]:text-gray-11",
-          actionButton: "group-[.toast]:bg-brand/15 group-[.toast]:text-brand",
+          actionButton: "group-[.toast]:bg-brand group-[.toast]:text-white",
           cancelButton: "group-[.toast]:bg-gray-a3 group-[.toast]:text-gray-a11",
         },
       }}
